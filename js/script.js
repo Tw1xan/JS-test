a3dd594fd33bbfa61015440bd089eed62f48542c
Tw1xan <tw1xan@yandex.ru>
2022-10-13 09:06:30 +0300

    day 3, add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 'use strics';
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  blackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IeMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.blackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+} else {
+  document.body.classList.add('_pc');
+}
